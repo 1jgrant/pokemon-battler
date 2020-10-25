7ad7a0610dcefba915a32cb2b581465b599dcc90
james grant <grantja92@gmail.com>
2020-10-25 22:58:44 +0000

    added changePokemon method and tests

--- _tests_/trainer.test.js
+++ _tests_/trainer.test.js
@@ -7,10 +7,56 @@ describe("Trainer class", () => {
   describe("PROPERTIES", () => {
     test("instance of the trainer class should have all required properties with default values", () => {
       const testTrainer = new Trainer();
+      const testTrainer2 = new Trainer(3, "gary");
       expect(testTrainer).toHaveProperty("name", "ash");
       expect(testTrainer).toHaveProperty("max", 6);
       expect(testTrainer).toHaveProperty("team", []);
       expect(testTrainer).toHaveProperty("items", []);
+      expect(testTrainer).toHaveProperty("activePokemon", undefined);
+
+      expect(testTrainer2).toHaveProperty("name", "gary");
+      expect(testTrainer2).toHaveProperty("max", 3);
+    });
+  });
+  describe("METHODS", () => {
+    test("catch - should place an instance of the specified pokemon in the Trainer instance's team", () => {
+      const testTrainer = new Trainer();
+      testTrainer.catch("squirtle", 15);
+      expect(testTrainer.team[0].name).toBe("squirtle");
+    });
+    test("catch - should can place multiple instances of the same class into the team", () => {
+      const testTrainer = new Trainer();
+      testTrainer.catch("squirtle", 15);
+      testTrainer.catch("squirtle", 30);
+      expect(testTrainer.team[0].level).toBe(15);
+      expect(testTrainer.team[1].level).toBe(30);
+      expect(testTrainer.team[0]).not.toBe(testTrainer.team[1]);
+    });
+    test("catch - should not add to the team if team is full", () => {
+      const testTrainer = new Trainer(3);
+      testTrainer.catch("squirtle", 15);
+      testTrainer.catch("squirtle", 30);
+      testTrainer.catch("charmander", 35);
+      testTrainer.catch("bulbasaur", 99);
+      expect(testTrainer.team.length).toBe(3);
+    });
+    test("changePokemon - should change the active pokemon", () => {
+      const testTrainer = new Trainer(3);
+      testTrainer.catch("squirtle", 15);
+      testTrainer.catch("squirtle", 30);
+      testTrainer.catch("charmander", 35);
+      expect(testTrainer.activePokemon).toBe(testTrainer.team[0]);
+      testTrainer.changePokemon(2);
+      expect(testTrainer.activePokemon).toBe(testTrainer.team[2]);
+    });
+    test("changePokemon - may only be used to select pokemon that have not fainted", () => {
+      const testTrainer = new Trainer(3);
+      testTrainer.catch("squirtle", 15);
+      testTrainer.catch("squirtle", 30);
+      testTrainer.catch("charmander", 35);
+      testTrainer.team[1].hp = 0;
+      testTrainer.changePokemon(1);
+      expect(testTrainer.activePokemon).toBe(testTrainer.team[0]);
     });
   });
 });
--- trainer-class.js
+++ trainer-class.js
@@ -1,9 +1,34 @@
+const Pokemon = require("./pokemon-class");
+
 class Trainer {
-  constructor(name, max) {
+  constructor(max = 6, name = "ash") {
     this.name = name;
     this.max = max;
     this.team = [];
     this.items = [];
+    this.activePokemon = this.team[0];
+  }
+
+  catch(pokemon, level) {
+    if (this.team.length < this.max) {
+      const newPoke = new Pokemon(pokemon, level);
+      newPoke.initStats();
+      this.team.push(newPoke);
+    } else {
+      //console.log("Team is full!");
+    }
+    //if chosenPokemon has not been selected, update to first pokemon in team
+    if (!this.activePokemon) this.activePokemon = this.team[0];
+  }
+
+  changePokemon(slot) {
+    if (slot > -1 && slot < this.team.length && this.team[slot].hp > 0) {
+      this.activePokemon = this.team[slot];
+    } else if (this.team[slot].hp < 1) {
+      //console.log(`${this.team[slot].name} is unconscious`);
+    } else {
+      //console.log("no pokemon there");
+    }
   }
 }
 
