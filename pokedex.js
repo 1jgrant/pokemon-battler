521eea4e0400c6cb5fded57fdc9cd87834dd419d
james grant <grantja92@gmail.com>
2020-10-25 14:49:48 +0000

    created templates for 4 initial pokemon

---
+++ pokedex.js
@@ -0,0 +1,60 @@
+//stats from:
+//https://pokemondb.net/pokedex/game/red-blue-yellow
+
+const pokedex = {
+  bulbasaur: {
+    hp: 45,
+    att: 49,
+    def: 49,
+    spAtt: 65,
+    spDef: 65,
+    speed: 45,
+    type: "grass",
+    moves: {
+      1: ["tackle"], //growl
+      7: ["leech seed"],
+      13: ["vine whip"],
+    },
+  },
+  charmander: {
+    hp: 39,
+    att: 52,
+    def: 43,
+    spAtt: 60,
+    spDef: 50,
+    speed: 65,
+    type: "fire",
+    moves: {
+      1: ["scratch"], //growl
+      9: ["ember"],
+    },
+  },
+  squirtle: {
+    hp: 44,
+    att: 48,
+    def: 65,
+    spAtt: 50,
+    spDef: 64,
+    speed: 43,
+    type: "water",
+    moves: {
+      1: ["tackle"], //tail whip
+      8: ["bubble"],
+      15: ["water gun"],
+    },
+  },
+  rattata: {
+    hp: 30,
+    att: 56,
+    def: 35,
+    spAtt: 25,
+    spDef: 35,
+    speed: 72,
+    type: "normal",
+    moves: {
+      1: ["tackle"], //tail whip
+      7: ["quick attack"],
+      14: ["hyper fang"],
+    },
+  },
+};
