ac953819126324fa563ec06ecd9a2f35559bdb4b
james grant <grantja92@gmail.com>
2020-11-07 19:34:38 +0000

    added useMove calls to the fight method

--- _tests_/battle.test.js
+++ _tests_/battle.test.js
@@ -16,8 +16,54 @@ describe("Battle class", () => {
         const testBattle = new Battle(testTrainer1, testTrainer2);
         testBattle.selectMove(testTrainer1, "bubble");
         testBattle.selectMove(testTrainer2, "scratch");
-        expect(testBattle.nextMove1.name).toEqual("bubble");
-        expect(testBattle.nextMove2.name).toEqual("scratch");
+        expect(testBattle.t1.nextMove.name).toEqual("bubble");
+        expect(testBattle.t2.nextMove.name).toEqual("scratch");
+      });
+    });
+    describe("useMove", () => {
+      test("method should use the attacker's move on the defender, subtracting the correct amount of damage for normal type moves", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 15);
+        testTrainer2.catch("squirtle", 15);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "scratch");
+        testBattle.selectMove(testTrainer2, "tackle");
+        testBattle.useMove(testTrainer1, testTrainer2);
+        expect(testTrainer2.activePokemon.hp).toBe(31);
+      });
+      test("method should use the attacker's move on the defender, subtracting the correct amount of damage for special type moves", () => {
+        const test1 = new Trainer(1, "trainer1");
+        const test2 = new Trainer(1, "trainer2");
+        test1.catch("squirtle", 15);
+        test2.catch("charmander", 15);
+        const testBattle = new Battle(test1, test2);
+        testBattle.selectMove(test1, "bubble");
+        testBattle.selectMove(test2, "ember");
+        testBattle.useMove(test1, test2);
+        expect(test2.activePokemon.hp).toBe(20);
+      });
+      test("method should reduce the attacker's move PP by 1", () => {
+        const test1 = new Trainer(1, "trainer1");
+        const test2 = new Trainer(1, "trainer2");
+        test1.catch("squirtle", 15);
+        test2.catch("charmander", 15);
+        const testBattle = new Battle(test1, test2);
+        testBattle.selectMove(test1, "bubble");
+        testBattle.selectMove(test2, "ember");
+        testBattle.useMove(test1, test2);
+        expect(test1.team[0].moves.bubble.currentPP).toBe(29);
+      });
+      test("pokemon health should be a minimum of 0", () => {
+        const test1 = new Trainer(1, "trainer1");
+        const test2 = new Trainer(1, "trainer2");
+        test1.catch("squirtle", 100);
+        test2.catch("charmander", 5);
+        const testBattle = new Battle(test1, test2);
+        testBattle.selectMove(test1, "bubble");
+        testBattle.selectMove(test2, "ember");
+        testBattle.useMove(test1, test2);
+        expect(test2.team[0].hp).toBe(0);
       });
     });
     describe("fight", () => {
@@ -40,13 +86,45 @@ describe("Battle class", () => {
         const testBattle = new Battle(testTrainer1, testTrainer2);
         testBattle.selectMove(testTrainer1, "ember");
         testBattle.selectMove(testTrainer2, "tackle");
-        console.log(testBattle);
         testBattle.fight();
         expect(testBattle.order[0].name).toBe("trainer2");
       });
-    });
-    describe("useMove", () => {
-      test("method should ", () => {});
+      test("method should use both moves", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 15);
+        testTrainer2.catch("rattata", 15);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "ember");
+        testBattle.selectMove(testTrainer2, "tackle");
+        testBattle.fight();
+        expect(testTrainer1.activePokemon.hp).not.toBe(36);
+        expect(testTrainer2.activePokemon.hp).not.toBe(34);
+      });
+      test("if first defender faints, next move should not be taken", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 80);
+        testTrainer2.catch("rattata", 15);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "ember");
+        testBattle.selectMove(testTrainer2, "tackle");
+        testBattle.fight();
+        expect(testTrainer1.activePokemon.hp).toBe(152);
+        expect(testTrainer2.activePokemon.hp).toBe(0);
+      });
+      test("if second defender faints, next move should not be taken", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 10);
+        testTrainer2.catch("rattata", 90);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "ember");
+        testBattle.selectMove(testTrainer2, "tackle");
+        testBattle.fight();
+        expect(testTrainer2.activePokemon.hp).toBe(154);
+        expect(testTrainer1.activePokemon.hp).toBe(0);
+      });
     });
   });
 });
--- battle-class.js
+++ battle-class.js
@@ -16,41 +16,78 @@ has defender fainted? if so,
     *** trainer picks new active pokemon if none left, other trainer has won ***
         *** if pokemon are available, bring out new pokemon & restart loop ***
 otherwise, restart loop
+
+-- damage calculations: 
+https://bulbapedia.bulbagarden.net/wiki/Damage#Damage_calculation
 */
+const { types, matrix } = require("./types");
 
 class Battle {
   constructor(trainer1, trainer2) {
     this.t1 = trainer1;
     this.t2 = trainer2;
     this.order = [this.t1, this.t2];
-    this.nextMove1;
-    this.nextMove2;
   }
   // method for each trainer to select their move
   selectMove(trainer, move) {
     // display available actions - attack, item, change pokemon
     if (trainer === this.t1) {
-      this.nextMove1 = this.t1.activePokemon.moves[move];
+      this.t1.nextMove = this.t1.activePokemon.moves[move];
     } else if (trainer === this.t2) {
-      this.nextMove2 = this.t2.activePokemon.moves[move];
+      this.t2.nextMove = this.t2.activePokemon.moves[move];
     }
   }
 
-  useMove(attacker, defender, move) {}
+  useMove(attacker, defender) {
+    // set the attack/defence ratio based on the move's category
+    let adRatio;
+    if (attacker.nextMove.category === "physical") {
+      adRatio = attacker.activePokemon.att / defender.activePokemon.def;
+    } else if (attacker.nextMove.category === "special") {
+      adRatio = attacker.activePokemon.spAtt / defender.activePokemon.spDef;
+    }
+    // calculate base damage
+    const baseDmg = Math.floor(
+      (((2 * attacker.activePokemon.level) / 5 + 2) *
+        attacker.nextMove.power *
+        adRatio) /
+        50 +
+        2
+    );
+    // multipliers can be accessed via matrix[attackerIndex][defenderIndex]
+    const typeMod =
+      matrix[types.indexOf(attacker.nextMove.type)][
+        types.indexOf(defender.activePokemon.type)
+      ];
+    // can also implement luck modifier
+    const totalDmg = baseDmg * typeMod;
+    defender.activePokemon.hp -= totalDmg;
+    if (defender.activePokemon.hp < 0) {
+      defender.activePokemon.hp = 0;
+      defender.activePokemon.isConscious = false;
+    }
+    attacker.nextMove.currentPP--;
+  }
 
   fight() {
     // trainer's moves are already decided
-    let first, second;
-    // console.log(this.t1);
-    // console.log(this.t2);
     //check which move should go first
     this.order.sort(() => {
-      if (this.nextMove1.priority < this.nextMove2.priority) return -1;
-      if (this.nextMove1.priority > this.nextMove2.priority) return 1;
+      if (this.t1.nextMove.priority < this.t2.nextMove.priority) return -1;
+      if (this.t1.nextMove.priority > this.t2.nextMove.priority) return 1;
       // If priority is the same, sort on pokemon speed
       if (this.t1.activePokemon.speed < this.t2.activePokemon.speed) return -1;
       if (this.t1.activePokemon.speed > this.t2.activePokemon.speed) return 1;
     });
+    this.useMove(this.order[0], this.order[1]);
+    if (this.order[1].activePokemon.isConscious) {
+      this.useMove(this.order[1], this.order[0]);
+      if (!this.order[0].activePokemon.isConscious) {
+        console.log(`${this.order[0].activePokemon.name} fainted`);
+      }
+    } else {
+      console.log(`${this.order[1].activePokemon.name} fainted`);
+    }
   }
 }
 
