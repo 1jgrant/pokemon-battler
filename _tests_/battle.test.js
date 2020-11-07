4ba4fd57485aaaa1be2981f734adc64b2d2ce9b5
james grant <grantja92@gmail.com>
2020-11-07 17:46:09 +0000

    working on batlle class, moving nextMove into the trainer object

--- _tests_/battle.test.js
+++ _tests_/battle.test.js
@@ -1,9 +1,52 @@
 const Battle = require("../battle-class");
+const Trainer = require("../trainer-class");
 
 describe("Battle class", () => {
   test("Battle class should return an object", () => {
     expect(typeof new Battle()).toBe("object");
   });
   describe("PROPERTIES", () => {});
-  describe("METHODS", () => {});
+  describe("METHODS", () => {
+    describe("selectMove", () => {
+      test("method should update the nextMove property for the desired trainer", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("squirtle", 15);
+        testTrainer2.catch("charmander", 15);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "bubble");
+        testBattle.selectMove(testTrainer2, "scratch");
+        expect(testBattle.nextMove1.name).toEqual("bubble");
+        expect(testBattle.nextMove2.name).toEqual("scratch");
+      });
+    });
+    describe("fight", () => {
+      test("method should calculate moves order based on move priority", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 50);
+        testTrainer2.catch("rattata", 15);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "ember");
+        testBattle.selectMove(testTrainer2, "quick attack");
+        testBattle.fight();
+        expect(testBattle.order[0].name).toBe("trainer2");
+      });
+      test("when move priority is the same, method should calculate fight order based on pokemon speed", () => {
+        const testTrainer1 = new Trainer(1, "trainer1");
+        const testTrainer2 = new Trainer(1, "trainer2");
+        testTrainer1.catch("charmander", 10);
+        testTrainer2.catch("rattata", 25);
+        const testBattle = new Battle(testTrainer1, testTrainer2);
+        testBattle.selectMove(testTrainer1, "ember");
+        testBattle.selectMove(testTrainer2, "tackle");
+        console.log(testBattle);
+        testBattle.fight();
+        expect(testBattle.order[0].name).toBe("trainer2");
+      });
+    });
+    describe("useMove", () => {
+      test("method should ", () => {});
+    });
+  });
 });
--- battle-class.js
+++ battle-class.js
@@ -21,7 +21,36 @@ otherwise, restart loop
 class Battle {
   constructor(trainer1, trainer2) {
     this.t1 = trainer1;
-    this.t2 - trainer2;
+    this.t2 = trainer2;
+    this.order = [this.t1, this.t2];
+    this.nextMove1;
+    this.nextMove2;
+  }
+  // method for each trainer to select their move
+  selectMove(trainer, move) {
+    // display available actions - attack, item, change pokemon
+    if (trainer === this.t1) {
+      this.nextMove1 = this.t1.activePokemon.moves[move];
+    } else if (trainer === this.t2) {
+      this.nextMove2 = this.t2.activePokemon.moves[move];
+    }
+  }
+
+  useMove(attacker, defender, move) {}
+
+  fight() {
+    // trainer's moves are already decided
+    let first, second;
+    // console.log(this.t1);
+    // console.log(this.t2);
+    //check which move should go first
+    this.order.sort(() => {
+      if (this.nextMove1.priority < this.nextMove2.priority) return -1;
+      if (this.nextMove1.priority > this.nextMove2.priority) return 1;
+      // If priority is the same, sort on pokemon speed
+      if (this.t1.activePokemon.speed < this.t2.activePokemon.speed) return -1;
+      if (this.t1.activePokemon.speed > this.t2.activePokemon.speed) return 1;
+    });
   }
 }
 
